fcb9f1818359d928f1f4b97ec7cfce451ebee01c
Kethan Flores <kethan.h.flores@gmail.com>
2023-09-26 16:27:01 +0200

    im working on it

--- 003_LargestPrimeFactor.py
+++ 003_LargestPrimeFactor.py
@@ -1,12 +1,14 @@
 import math
 from tqdm import tqdm
 CONST_NUM = 600851475143
-"""
+
 # generate primes until 1/2 of CONST_NUM
 # check through array if CONST_NUM % prime = 0
 # end search when array is complete
+
+"""
 primes = [2]
-for num in tqdm(range(3,1_000_000)):
+for num in tqdm(range(3,9999999)):
     composite = False
     for prime in primes:
         if prime < math.sqrt(num):
@@ -18,4 +20,18 @@ for num in tqdm(range(3,1_000_000)):
     if composite == False:
         primes.append(num)
 print(primes)
+
 """
+primeFactors = []
+num = 69
+while num > 0:
+    if num % 2 == 0:
+        num /= 2
+        primeFactors.append(2)
+    elif num == 1:
+        break
+    else:
+        primeFactors.append(num)
+        break
+
+print(primeFactors)
